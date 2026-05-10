3b9901311af6a39ee15f7a8e819973693aa80eff
Max Gfeller <mg@cyon.ch>
2026-05-10 11:02:19 +0200

    Remove web frontend

--- src/lib/types.ts
+++
@@ -1,9 +0,0 @@
-export interface TreeNode {
-  name: string;
-  path: string;
-  size: number;
-  type: "file" | "directory";
-  extension?: string;
-  children?: TreeNode[];
-  truncated?: boolean;
-}
